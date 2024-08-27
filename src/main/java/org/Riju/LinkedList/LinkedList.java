c7e18ff3ddda642cde968c00df38058caa8f6de6
Riju Mondal <contact.rijumondal@gmail.com>
2024-08-27 20:01:33 +0530

    Linked List

--- src/main/java/org/Riju/LinkedList/LinkedList.java
+++ src/main/java/org/Riju/LinkedList/LinkedList.java
@@ -1,4 +1,89 @@
 package org.Riju.LinkedList;
 
-public class LinkedList {
+public class LinkedList<E>{
+    Node<E> head;
+    Node<E> tail;
+    int size;
+
+
+    public static class Node<E>{
+        E data;
+        Node<E> next;
+
+         Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+        size = 0;
+    }
+
+    public void add(E data){
+
+        Node newNode = new Node<>(data,null);
+        if(tail == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    public E get(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        Node<E> curr = head;
+
+        for(int i = 0 ; i < index; i++){
+            curr = curr.next;
+        }
+
+        return curr.data;
+    }
+
+
+    public void remove(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        if(index == 0) {
+            head = head.next;
+            if (head == null) {
+                head = null;
+                tail = null;
+            }
+        }
+        else{
+                Node<E> prev = head;
+                for(int i = 0 ; i < index-1; i++){
+                    prev = prev.next;
+                }
+
+                prev.next = prev.next.next;
+
+                if(prev.next == null){
+                    tail = prev;
+                }
+            }
+
+        size--;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
 }
--- src/main/java/org/Riju/Main.java
+++ src/main/java/org/Riju/Main.java
@@ -1,17 +1,24 @@
-package org.Riju;
+ package org.Riju;
+ import org.Riju.LinkedList.LinkedList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.add(20);
+        ll.add(90);
+        ll.add(40);
+        ll.add(50);
+
+        System.out.println("Element at index 1 is " + ll.get(1));
+        System.out.println("isEmpty() " + ll.isEmpty());
+        System.out.println("Get Element " + ll.get(2));
+
+        ll.remove(1);
+        System.out.println("Get Element " + ll.get(1));
+
+
     }
 }
